6fc13a75776fd8a9f11f03a1791e4c3484f9d290
yuna096740 <yyuusseeii1111@gmail.com>
2023-09-17 23:28:17 +0000

    [add] chalendar layouts

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,12 +12,6 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
-
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
                         {{ __('Schedule') }}
                     </x-nav-link>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,31 +1,36 @@
-@extends('layouts/app')
 @section('title', 'スケジュール')
 @include('posts.createScheduleModal')
 @include('posts.clickScheduleModal')
 @include('posts.editScheduleModal')
 
-@section('content')
-<div class="container mx-auto">
-
-  <div class="flex justify-between mt-2">
-    
-    <div>
-      <button class="rounded bg-gray-500 text-white hover:bg-gray-700 px-5 py-2" data-micromodal-trigger="createScheduleModal">新規作成</button>
+<x-app-layout>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ __('Schedule') }}
+    </h2>
+  </x-slot>
+  <div class="container mx-auto">
+  
+    <div class="flex justify-between mt-5">
+      
+      <div>
+        <button class="rounded bg-gray-500 text-white hover:bg-gray-700 px-5 py-2" data-micromodal-trigger="createScheduleModal">新規作成</button>
+      </div>
+      
+      <form>
+        <button id="clearButton" style="display: none;" class="mt-3 text-gray-500">リセットする</button>
+        
+        <input type="text" id="searchSchedule" placeholder="イベントを検索する" class="w-60 mx-3 rounded border-b focus:outline-none focus:border-b-2 focus:border-indigo-500 placeholder-gray-500 placeholder-opacity-50">
+        <input type="date" id="searchDate">
+        
+        <button id="searchButton" class="rounded bg-gray-500 text-white hover:bg-gray-700 ml-3 px-5 py-2">検索</button>
+      </form>
     </div>
     
-    <form>
-      <button id="clearButton" style="display: none;" class="mt-3 text-gray-500">リセットする</button>
-      
-      <input type="text" id="searchSchedule" placeholder="イベントを検索する" class="w-60 mx-3 rounded border-b focus:outline-none focus:border-b-2 focus:border-indigo-500 placeholder-gray-500 placeholder-opacity-50">
-      <input type="date" id="searchDate">
-      
-      <button id="searchButton" class="rounded bg-gray-500 text-white hover:bg-gray-700 ml-3 px-5 py-2">検索</button>
-    </form>
+    <div id="calendar" ></div>
+    
   </div>
-  
-  <div id="calendar" ></div>
-  
-</div>
+</x-app-layout>
 
 <script src="https://unpkg.com/micromodal/dist/micromodal.min.js"></script>
 <script>
@@ -35,5 +40,3 @@
     awaitCloseAnimation: true
   });
 </script>
-
-@endsection
