4d21fc82c2c2fcd8c5820085a9c495688923a39c
yuna096740 <yyuusseeii1111@gmail.com>
2023-09-18 06:18:28 +0000

    [add] profile layouts

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-sky-200">
+<div class="bg-glay-400">
   <a href="https://github.com/yuna096740/Onschetto">
     <p class="py-5 text-center text-xs">
       @Onschetto createdBy Yuna096740
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,20 +11,20 @@
   </x-slot>
   <div class="container mx-auto">
   
-    <div class="flex justify-between mt-5">
+    <div class="flex justify-between my-5">
       
       <div>
-        <button class="rounded bg-gray-500 text-white hover:bg-gray-700 px-5 py-2" data-micromodal-trigger="createScheduleModal">新規作成</button>
+        <button class="rounded bg-glay-400 border text-darck hover:bg-gray-200 px-5 py-2" data-micromodal-trigger="createScheduleModal">新規作成</button>
       </div>
       
-      <form>
+      <div>
         <button id="clearButton" style="display: none;" class="mt-3 text-gray-500">リセットする</button>
         
         <input type="text" id="searchSchedule" placeholder="イベントを検索する" class="w-60 mx-3 rounded border-b focus:outline-none focus:border-b-2 focus:border-indigo-500 placeholder-gray-500 placeholder-opacity-50">
         <input type="date" id="searchDate">
         
-        <button id="searchButton" class="rounded bg-gray-500 text-white hover:bg-gray-700 ml-3 px-5 py-2">検索</button>
-      </form>
+        <button id="searchButton" class="rounded bg-glay-400 border text-darck hover:bg-gray-200 ml-3 px-5 py-2">検索</button>
+      </div>
     </div>
     
     <div id="calendar" ></div>
